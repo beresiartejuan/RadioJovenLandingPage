82121bb704dacc9c077c27adbc5ef78216c750c3
juan beresiarte <juanberesiarte@gmail.com>
2024-10-29 15:14:46 -0300

    Navbar responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,1 +1,22 @@
-export default function () { }
+import style from "../scss/Navbar.module.scss";
+import MenuIcon from "../assets/MenuIcon.jsx";
+import { useState } from "react";
+
+export default function Navbar() {
+
+    const [isOpen, setOpen] = useState(false);
+
+    return (
+        <nav className={style.navbar}>
+            <div>
+                <img className="logo" src="/logo.jpeg"></img>
+                <MenuIcon onClick={() => setOpen(!isOpen)} />
+            </div>
+            <div className={`${isOpen ? style.show : ''}`}>
+                <a className="item" href="">Inicio</a>
+                <a className="item" href="">Horoscopo</a>
+                <a className="item" href="">Eventos</a>
+            </div>
+        </nav>
+    )
+}
